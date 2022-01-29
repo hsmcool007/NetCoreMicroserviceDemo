94f41d3f42c525cbddc5d7930f0045a68c40c9d4
Simon Huang <simon.huang@carestream.com>
2022-01-29 22:29:25 +0800

    first commit

---
+++ Order.API/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Order.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+
+        private readonly ILogger<OrdersController> _logger;
+      
+        public OrdersController(ILogger<OrdersController> logger)
+        {
+            _logger = logger;
+    
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string result = $"【订单服务】{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}——" +
+                $"{Request.HttpContext.Connection.LocalIpAddress}:{Request.HttpContext.Connection.LocalPort}";
+            return Ok(result);
+        }
+
+
+    }
+}
