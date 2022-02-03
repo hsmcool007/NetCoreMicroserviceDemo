ae08086f11fde5c31edb3270e40a0bd2e40b3722
Simon Huang <simon.huang@carestream.com>
2022-02-04 01:51:02 +0800

    commit

--- Ocelot.APIGateway/Startup.cs
+++ Ocelot.APIGateway/Startup.cs
@@ -1,10 +1,11 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +19,7 @@ namespace Ocelot.APIGateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot();
+            services.AddOcelot().AddConsul();//添加consul支持
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
