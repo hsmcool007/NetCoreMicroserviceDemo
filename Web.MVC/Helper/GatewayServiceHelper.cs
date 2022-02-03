3926e44655a0e9fa367ebf5692c2248e718d2603
Simon Huang <simon.huang@carestream.com>
2022-02-04 00:54:23 +0800

    commit

---
+++ Web.MVC/Helper/GatewayServiceHelper.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.MVC.Helper
+{
+    /// <summary>
+    /// 通过gateway调用服务
+    /// </summary>
+    public class GatewayServiceHelper : IServiceHelper
+    {
+        public async Task<string> GetOrder()
+        {
+            var Client = new RestClient("http://10.112.9.230:9080");
+            var request = new RestRequest("/api/orders", Method.Get);
+
+            var response = await Client.ExecuteAsync(request);
+            return response.Content;
+        }
+
+        public async Task<string> GetProduct()
+        {
+            var Client = new RestClient("http://10.112.9.230:9080");
+            var request = new RestRequest("/api/products", Method.Get);
+
+            var response = await Client.ExecuteAsync(request);
+            return response.Content;
+        }
+
+        public void GetServices()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Web.MVC/Startup.cs
+++ Web.MVC/Startup.cs
@@ -24,7 +24,7 @@ namespace Web.MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddSingleton<IServiceHelper, ServiceHelper>();
+            services.AddSingleton<IServiceHelper, GatewayServiceHelper>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
